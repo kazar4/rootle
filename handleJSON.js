0f7258bcf2a3371aa51552f317ded2e99b28786b
Kazen Gallman <kazar444@icloud.com>
2022-12-04 20:25:50 -0500

    using secure fetches

--- handleJSON.js
+++ handleJSON.js
@@ -1,18 +1,18 @@
 //receives a JSON file of all the data small businesses similiar to the one given
 function getBizSearch(address, biz){
-    URL = 'http://127.0.0.1:5000/getFromBusiness/?loc=' + address + "&bbs=" + biz;
+    URL = 'https://127.0.0.1:5000/getFromBusiness/?loc=' + address + "&bbs=" + biz;
     JSON.parse(httpGet(URL));
 }
 
 //receives a JSON file of all the small business with the query
 function getCustomSearch(address, query){
-    URL = 'http://127.0.0.1:5000/getFull/?loc=' + address + "&inp=" + query;
+    URL = 'https://127.0.0.1:5000/getFull/?loc=' + address + "&inp=" + query;
     JSON.parse(httpGet(URL));
 }
 
 //gets more details from a specific location
 function getSpecficLocation(Location_ID){
-    URL = 'http://127.0.0.1:5000/getBusiness/' + Location_ID;
+    URL = 'https://127.0.0.1:5000/getBusiness/' + Location_ID;
     JSON.parse(httpGet(URL));
 }
 
@@ -34,14 +34,14 @@ function getData(){
     console.log(toggle_button);
     //Search with big business
     if (!toggle_button) {
-        URL = 'http://127.0.0.1:5000/getFromBusiness/?loc=' + address + "&bbs=" + business_category;
+        URL = 'https://127.0.0.1:5000/getFromBusiness/?loc=' + address + "&bbs=" + business_category;
         business_data = getBizSearch(address, business_category);
         displayPlaces(business_data);
         console.log("trying to display1");
 
     //General Search
     } else {
-        URL = 'http://127.0.0.1:5000/getFromBusiness/?loc=' + address + "&inp=" + business_category;
+        URL = 'https://127.0.0.1:5000/getFromBusiness/?loc=' + address + "&inp=" + business_category;
         business_data = getCustomSearch(address, business_category);
         displayPlaces(business_data);
         console.log("trying to display2");
