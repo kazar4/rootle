b13646c5d172fd80e78f5db6c6e75bb981d7f3fa
Kazen Gallman <kazar444@icloud.com>
2022-12-04 20:31:07 -0500

    fetches should use correct path now

--- handleJSON.js
+++ handleJSON.js
@@ -1,18 +1,24 @@
+
+let address = 'https://' + document.location.host;
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+    address = 'https://127.0.0.1:5000';
+}
+
 //receives a JSON file of all the data small businesses similiar to the one given
 function getBizSearch(address, biz){
-    URL = 'https://127.0.0.1:5000/getFromBusiness/?loc=' + address + "&bbs=" + biz;
+    URL = address + '/getFromBusiness/?loc=' + address + "&bbs=" + biz;
     JSON.parse(httpGet(URL));
 }
 
 //receives a JSON file of all the small business with the query
 function getCustomSearch(address, query){
-    URL = 'https://127.0.0.1:5000/getFull/?loc=' + address + "&inp=" + query;
+    URL = address + '/getFull/?loc=' + address + "&inp=" + query;
     JSON.parse(httpGet(URL));
 }
 
 //gets more details from a specific location
 function getSpecficLocation(Location_ID){
-    URL = 'https://127.0.0.1:5000/getBusiness/' + Location_ID;
+    URL = address + '/getBusiness/' + Location_ID;
     JSON.parse(httpGet(URL));
 }
 
@@ -34,14 +40,14 @@ function getData(){
     console.log(toggle_button);
     //Search with big business
     if (!toggle_button) {
-        URL = 'https://127.0.0.1:5000/getFromBusiness/?loc=' + address + "&bbs=" + business_category;
+        URL = address + '/getFromBusiness/?loc=' + address + "&bbs=" + business_category;
         business_data = getBizSearch(address, business_category);
         displayPlaces(business_data);
         console.log("trying to display1");
 
     //General Search
     } else {
-        URL = 'https://127.0.0.1:5000/getFromBusiness/?loc=' + address + "&inp=" + business_category;
+        URL = address + '/getFromBusiness/?loc=' + address + "&inp=" + business_category;
         business_data = getCustomSearch(address, business_category);
         displayPlaces(business_data);
         console.log("trying to display2");
